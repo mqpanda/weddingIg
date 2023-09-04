83189f6a1a598bcf25366eca935bbdb852abd0ef
Daniil Praktika <daniilpraktika@gmail.com>
2023-09-04 23:20:02 +0300

    seventh commit

---
+++ telegram.php
@@ -0,0 +1,29 @@
+<?php
+
+/* https://api.telegram.org/bot5186120091:AAE6MBfdLE3tJCaCh6xvBEVwIQ09Io6GArg/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['na2130me'];
+$presence = $_POST['for223m'];
+$dop = $_POST['d321op'];
+
+$token = "6271125696:AAF5jsdq1pengjYvfzw_LEqWLHboilT2BZ0";
+$chat_id = "711746679";
+$arr = array(
+  'Кто ответил:' => $name,
+  'Присутствие: ' => $presence,
+  'Дополнительно: ' => $dop,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: ./thx.html');
+} else {
+  echo "Error";
+}
+?>
